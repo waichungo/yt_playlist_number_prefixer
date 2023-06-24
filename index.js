a8739cceeb2470aac3837f45d55a9726a8cd95d6
waichungo <waichungojames@gmail.com>
2023-06-24 11:13:21 +0300

    Code fix

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ if (args.length > 3) {
     var dir = args[2]
     var playlistId = args[3]
     if (fs.existsSync(dir)) {
-        var playlist=''
-        var files=fs.readdirSync(dir).map(e=>`${dir}/${e}`);
-        proc = spawn("yt-dlp.exe", ["--skip-download", "--flat-playlist","-J", playlistId])
-        
-        
+        var playlist = ''
+        var files = fs.readdirSync(dir).map(e => `${dir}/${e}`);
+        proc = spawn("yt-dlp.exe", ["--skip-download", "--flat-playlist", "-J", playlistId])
+
+
         proc.stdout.on('data', (data) => {
-            playlist+=data;
+            playlist += data;
         });
 
         proc.stderr.on('data', (data) => {
@@ -22,19 +22,19 @@ if (args.length > 3) {
         proc.on('close', (code) => {
             console.log(`child process exited with code ${code}`);
 
-            let list=JSON.parse(playlist)
-            let maxZeroes=`${files.length}`.length
-            let counter=0;
+            let list = JSON.parse(playlist)
+            let maxZeroes = `${files.length}`.length
+            let counter = 0;
             for (const video of list.entries) {
                 counter++
-                let id=video.id;
+                let id = video.id;
                 for (const file of files) {
-                    if(file.includes(id)){
-                        if(!/^\(\d+\)/gi.test(file)){
-                            let dir=path.dirname(file)
-                            let fBase=path.basename(file)
-                            let newName=`${dir}/(${counter.toString().padStart(maxZeroes,"0")}) ${fBase}`
-                            fs.renameSync(file,newName);
+                    if (file.includes(id)) {
+                        let dir = path.dirname(file)
+                        let fBase = path.basename(file)
+                        if (!/^\(\d+\)/gi.test(fBase)) {
+                            let newName = `${dir}/(${counter.toString().padStart(maxZeroes, "0")}) ${fBase}`
+                            fs.renameSync(file, newName);
                         }
                         break;
                     }
